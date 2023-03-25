a6d58ffa61d80eb213a3e24a5083cd93ec96921b
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-25 18:01:53 -0300

    create interface InsertCustomer

---
+++ src/main/java/com/fernandes/cleanarch/core/dataprovider/InsertCustomer.java
@@ -0,0 +1,7 @@
+package com.fernandes.cleanarch.core.dataprovider;
+
+import com.fernandes.cleanarch.core.domain.Customer;
+
+public interface InsertCustomer {
+    void insert(Customer customer);
+}
