ab938001060943a7ff02fbabf56b9a772ba5b1d1
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-25 22:42:36 -0300

    create CustomerRepository

---
+++ src/main/java/com/fernandes/cleanarch/dataprovider/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.fernandes.cleanarch.dataprovider.repository;
+
+import com.fernandes.cleanarch.dataprovider.repository.entity.CustomerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+}
