d3f74a3c946dce194676ebb72b3a6a987bcdd835
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-26 16:01:29 -0300

    implemented FindCustomerByIdUseCaseImpl to core

---
+++ src/main/java/com/fernandes/cleanarch/core/usecase/impl/FindCustomerByIdUseCaseImpl.java
@@ -0,0 +1,19 @@
+package com.fernandes.cleanarch.core.usecase.impl;
+
+import com.fernandes.cleanarch.core.dataprovider.FindCustomerById;
+import com.fernandes.cleanarch.core.domain.Customer;
+import com.fernandes.cleanarch.core.usecase.FindCustomerByIdUseCase;
+
+public class FindCustomerByIdUseCaseImpl implements FindCustomerByIdUseCase {
+    private FindCustomerById findCustomerById;
+
+    FindCustomerByIdUseCaseImpl(FindCustomerById findCustomerById){
+        this.findCustomerById = findCustomerById;
+    }
+
+    @Override
+    public Customer find(String id) {
+        return findCustomerById.find(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found!"))
+    }
+}
