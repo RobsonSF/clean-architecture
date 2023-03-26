b13ae48dfc1d9760fb36de95ab2d7edf38685c98
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-26 17:11:25 -0300

    implements UpdateCustomerUseCaseImpl to usecase in core

---
+++ src/main/java/com/fernandes/cleanarch/core/usecase/impl/UpdateCustomerUseCaseImpl.java
@@ -0,0 +1,30 @@
+package com.fernandes.cleanarch.core.usecase.impl;
+
+import com.fernandes.cleanarch.core.dataprovider.FindAddressByZipCode;
+import com.fernandes.cleanarch.core.dataprovider.UpdateCustomer;
+import com.fernandes.cleanarch.core.domain.Customer;
+import com.fernandes.cleanarch.core.usecase.FindCustomerByIdUseCase;
+import com.fernandes.cleanarch.core.usecase.UpdateCustomerUseCase;
+
+public class UpdateCustomerUseCaseImpl implements UpdateCustomerUseCase {
+    private FindCustomerByIdUseCase findCustomerByIdUseCase;
+    private FindAddressByZipCode findAddressByZipCode;
+    private UpdateCustomer updateCustomer;
+
+    public UpdateCustomerUseCaseImpl(
+            FindCustomerByIdUseCase findCustomerByIdUseCase,
+            FindAddressByZipCode findAddressByZipCode,
+            UpdateCustomer updateCustomer) {
+        this.findCustomerByIdUseCase = findCustomerByIdUseCase;
+        this.findAddressByZipCode = findAddressByZipCode;
+        this.updateCustomer = updateCustomer;
+    }
+
+    @Override
+    public void update(Customer customer, String zipCode) {
+        this.findCustomerByIdUseCase.find(customer.getId());
+        var address  = this.findAddressByZipCode.find(zipCode);
+        customer.setAddress(address);
+        this.updateCustomer.update(customer);
+    }
+}
