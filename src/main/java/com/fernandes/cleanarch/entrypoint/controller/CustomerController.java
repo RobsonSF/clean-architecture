96be9cab1e93d39bc3a2315e10fca994983e7d0a
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-26 16:53:28 -0300

    implements findCustomerById to controller

--- src/main/java/com/fernandes/cleanarch/entrypoint/controller/CustomerController.java
+++ src/main/java/com/fernandes/cleanarch/entrypoint/controller/CustomerController.java
@@ -1,14 +1,14 @@
 package com.fernandes.cleanarch.entrypoint.controller;
 
+import com.fernandes.cleanarch.core.usecase.FindCustomerByIdUseCase;
 import com.fernandes.cleanarch.core.usecase.InsertCustomerUseCase;
 import com.fernandes.cleanarch.entrypoint.controller.mapper.CustomerMapper;
 import com.fernandes.cleanarch.entrypoint.controller.request.CustomerRequest;
+import com.fernandes.cleanarch.entrypoint.controller.response.CustomerResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -18,11 +18,22 @@ public class CustomerController {
     private InsertCustomerUseCase insertCustomerUseCase;
 
     @Autowired
+    private FindCustomerByIdUseCase findCustomerByIdUseCase;
+
+    @Autowired
     private CustomerMapper customerMapper;
 
+    @PostMapping
     public ResponseEntity<Void> insert(@Valid @RequestBody CustomerRequest customerRequest) {
         var customer = customerMapper.toCustomer(customerRequest);
         insertCustomerUseCase.insert(customer, customerRequest.getZipCode());
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable final String id){
+        var customer = findCustomerByIdUseCase.find(id);
+        var customerResponse = customerMapper.toCustomerResponse(customer);
+        return ResponseEntity.ok().body(customerResponse);
     }
 }
