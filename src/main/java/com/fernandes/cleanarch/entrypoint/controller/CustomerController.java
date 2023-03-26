a7d7d510efcf8cd20f3530bf45d2c76ddd488f0f
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-25 23:45:12 -0300

    create CustomerController and implements endpoint insert

---
+++ src/main/java/com/fernandes/cleanarch/entrypoint/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.fernandes.cleanarch.entrypoint.controller;
+
+import com.fernandes.cleanarch.core.usecase.InsertCustomerUseCase;
+import com.fernandes.cleanarch.entrypoint.controller.mapper.CustomerMapper;
+import com.fernandes.cleanarch.entrypoint.controller.request.CustomerRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    @Autowired
+    private InsertCustomerUseCase insertCustomerUseCase;
+
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    public ResponseEntity<Void> insert(@Valid @RequestBody CustomerRequest customerRequest) {
+        var customer = customerMapper.toCustomer(customerRequest);
+        insertCustomerUseCase.insert(customer, customerRequest.getZipCode());
+        return ResponseEntity.ok().build();
+    }
+}
