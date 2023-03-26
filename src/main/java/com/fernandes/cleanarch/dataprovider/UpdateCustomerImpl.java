20bebc6c3907a7b78b91a436766f09e491e6a417
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-26 17:53:18 -0300

    create UpdateCustomerImpl to dataprovider

---
+++ src/main/java/com/fernandes/cleanarch/dataprovider/UpdateCustomerImpl.java
@@ -0,0 +1,24 @@
+package com.fernandes.cleanarch.dataprovider;
+
+import com.fernandes.cleanarch.core.dataprovider.UpdateCustomer;
+import com.fernandes.cleanarch.core.domain.Customer;
+import com.fernandes.cleanarch.dataprovider.repository.CustomerRepository;
+import com.fernandes.cleanarch.dataprovider.repository.mapper.CustomerEntityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class UpdateCustomerImpl implements UpdateCustomer {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void update(Customer customer) {
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+}
