a3eac715ffcee4ee40fca694d7424a07a921693d
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-26 17:10:06 -0300

    create interface UpdateCustomer to dataprovider in core

---
+++ src/main/java/com/fernandes/cleanarch/core/dataprovider/UpdateCustomer.java
@@ -0,0 +1,7 @@
+package com.fernandes.cleanarch.core.dataprovider;
+
+import com.fernandes.cleanarch.core.domain.Customer;
+
+public interface UpdateCustomer {
+    void update(Customer customer);
+}
