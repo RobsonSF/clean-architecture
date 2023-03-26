d32fa08d54bdb183f84c80c455f137b0b24885da
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-25 22:43:22 -0300

    create CustomerEntityMapper

---
+++ src/main/java/com/fernandes/cleanarch/dataprovider/repository/mapper/CustomerEntityMapper.java
@@ -0,0 +1,10 @@
+package com.fernandes.cleanarch.dataprovider.repository.mapper;
+
+import com.fernandes.cleanarch.core.domain.Customer;
+import com.fernandes.cleanarch.dataprovider.repository.entity.CustomerEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
+    CustomerEntity toCustomerEntity(Customer customer);
+}
