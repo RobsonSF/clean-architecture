de796e5d16f23be53d413b3b762b7b592a448f7a
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-26 18:59:33 -0300

    refactor imports DeleteCustomerByIdConfig

--- src/main/java/com/fernandes/cleanarch/config/DeleteCustomerByIdConfig.java
+++ src/main/java/com/fernandes/cleanarch/config/DeleteCustomerByIdConfig.java
@@ -1,7 +1,5 @@
 package com.fernandes.cleanarch.config;
 
-import com.fernandes.cleanarch.core.dataprovider.DeleteCustomerById;
-import com.fernandes.cleanarch.core.usecase.FindCustomerByIdUseCase;
 import com.fernandes.cleanarch.core.usecase.impl.DeleteCustomerByIdUseCaseImpl;
 import com.fernandes.cleanarch.core.usecase.impl.FindCustomerByIdUseCaseImpl;
 import com.fernandes.cleanarch.dataprovider.DeleteCustomerByIdImpl;
