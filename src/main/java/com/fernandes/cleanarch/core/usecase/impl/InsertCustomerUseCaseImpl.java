2c1ce52ad4d96c6069e8d0baf82b77860713659a
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-26 22:32:59 -0300

    implements SendCpfToValidationImpl

--- src/main/java/com/fernandes/cleanarch/core/usecase/impl/InsertCustomerUseCaseImpl.java
+++ src/main/java/com/fernandes/cleanarch/core/usecase/impl/InsertCustomerUseCaseImpl.java
@@ -2,6 +2,7 @@ package com.fernandes.cleanarch.core.usecase.impl;
 
 import com.fernandes.cleanarch.core.dataprovider.FindAddressByZipCode;
 import com.fernandes.cleanarch.core.dataprovider.InsertCustomer;
+import com.fernandes.cleanarch.core.dataprovider.SendCpfToValidation;
 import com.fernandes.cleanarch.core.domain.Customer;
 import com.fernandes.cleanarch.core.usecase.InsertCustomerUseCase;
 
@@ -9,12 +10,16 @@ public class InsertCustomerUseCaseImpl implements InsertCustomerUseCase {
     private final FindAddressByZipCode findAddressByZipCode;
     private final InsertCustomer insertCustomer;
 
+    private final SendCpfToValidation sendCpfToValidation;
+
     public InsertCustomerUseCaseImpl(
             FindAddressByZipCode findAddressByZipCode,
-            InsertCustomer insertCustomer
+            InsertCustomer insertCustomer,
+            SendCpfToValidation sendCpfToValidation
     ) {
         this.findAddressByZipCode = findAddressByZipCode;
         this.insertCustomer = insertCustomer;
+        this.sendCpfToValidation  = sendCpfToValidation;
     }
 
     @Override
@@ -22,5 +27,6 @@ public class InsertCustomerUseCaseImpl implements InsertCustomerUseCase {
         var address  = this.findAddressByZipCode.find(zipCode);
         customer.setAddress(address);
         this.insertCustomer.insert(customer);
+        this.sendCpfToValidation.send(customer.getCpf());
     }
 }
